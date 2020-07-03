43598dad86b9f75199a9e06973c573babc26bac5
Richard Mah <dick.mah@gmail.com>
2020-07-03 15:36:28 -0400

    Configuring environment

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name="iconetl",
+    version="0.0.1",
+    packages=[""],
+    url="",
+    license="",
+    author="Richard Mah",
+    author_email="richard@richardmah.com",
+    description="",
+    python_requires=">=3.6.0,<3.8.0",
+    install_requires=[
+        "blockchain-etl-common==1.3.0",
+        "requests==2.20.0",
+        "python-dateutil==2.7.0",
+        "click==7.0",
+        "iconsdk >= 1.3.2",
+    ],
+)
