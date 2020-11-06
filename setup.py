0bd93cc316c60ff1479fcd751e68bd12600b21ca
Richard Mah <dick.mah@gmail.com>
2020-11-06 10:56:47 -0500

    loosen requirements bump version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ long_description = read("README.md") if os.path.isfile("README.md") else ""
 
 setup(
     name="icon-etl",
-    version="0.1.2",
+    version="0.1.3",
     packages=find_packages(exclude=["schemas", "tests"]),
     url="https://github.com/insight-icon/icon-etl",
     author="Richard Mah",
@@ -22,14 +22,17 @@ setup(
     python_requires=">=3.6.0,<3.8.0",
     install_requires=[
         "blockchain-etl-common==1.3.0",
-        "requests==2.20.0",
-        "python-dateutil==2.7.0",
+        "requests>=2.20.0",
+        "python-dateutil>=2.7.0",
         "click==7.0",
         "iconsdk >= 1.3.2",
         "web3 >= 5.5.0",
     ],
     extras_require={
-        "streaming": ["timeout-decorator==0.4.1", "sqlalchemy==1.3.13",],
+        "streaming": [
+            "timeout-decorator==0.4.1",
+            "sqlalchemy==1.3.13",
+        ],
         "dev": ["pytest~=4.3.0"],
     },
     project_urls={
@@ -43,5 +46,9 @@ setup(
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
     ],
-    entry_points={"console_scripts": ["iconetl=iconetl.cli:cli",],},
+    entry_points={
+        "console_scripts": [
+            "iconetl=iconetl.cli:cli",
+        ],
+    },
 )
