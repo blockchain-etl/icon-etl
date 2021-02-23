ea4733a234cf225129e4546aeac7e5a4ef0d8e5a
Richard Mah <dick.mah@gmail.com>
2021-02-23 16:15:17 -0500

    Add Kafka streaming features (#4)

--- iconetl/streaming/item_exporter_creator.py
+++ iconetl/streaming/item_exporter_creator.py
@@ -25,7 +25,7 @@ from blockchainetl_common.jobs.exporters.console_item_exporter import (
 )
 
 
-def create_item_exporter(output):
+def create_item_exporter(output, kafka_settings):
     item_exporter_type = determine_item_exporter_type(output)
     if item_exporter_type == ItemExporterType.PUBSUB:
         from blockchainetl_common.jobs.exporters.google_pubsub_item_exporter import (
@@ -74,6 +74,76 @@ def create_item_exporter(output):
                 ListFieldItemConverter("topics", "topic", fill=4),
             ],
         )
+
+    elif item_exporter_type == ItemExporterType.KAFKA:
+        from json import dumps
+
+        from confluent_kafka import Producer
+        from confluent_kafka.admin import AdminClient
+        from confluent_kafka.schema_registry import SchemaRegistryClient
+
+        from iconetl.jobs.exporters.kafka_item_exporter import KafkaItemExporter
+        from iconetl.streaming.icx_json_schema import (
+            create_topic,
+            register_schema,
+            schema_exist,
+            topic_exist,
+        )
+
+        # Check for topics, if not exist, create them
+        admin_client = AdminClient({"bootstrap.servers": output})
+        for topic_type, topic_name in kafka_settings["topic_map"].items():
+            if not topic_exist(admin_client, topic_name):
+                create_topic(admin_client, topic_name)
+
+        # If schema registry enabled, verify correct schemas are registered
+        if kafka_settings["schema_registry_url"]:
+            from confluent_kafka.schema_registry.json_schema import JSONSerializer
+
+            from iconetl.streaming.icx_json_schema import get_schema
+
+            registry_client = SchemaRegistryClient(
+                {"url": kafka_settings["schema_registry_url"]}
+            )
+
+            serializers = {
+                "block": JSONSerializer(
+                    get_schema(kafka_settings["topic_map"]["block"], "block"),
+                    registry_client,
+                    conf={"auto.register.schemas": False},
+                ),
+                "transaction": JSONSerializer(
+                    get_schema(
+                        kafka_settings["topic_map"]["transaction"], "transaction"
+                    ),
+                    registry_client,
+                    conf={"auto.register.schemas": False},
+                ),
+                "log": JSONSerializer(
+                    get_schema(kafka_settings["topic_map"]["log"], "log"),
+                    registry_client,
+                    conf={"auto.register.schemas": False},
+                ),
+            }
+
+            for topic_type, topic_name in kafka_settings["topic_map"].items():
+                if not schema_exist(registry_client, topic_name, topic_type):
+                    register_schema(registry_client, topic_name, topic_type)
+
+        else:
+            serializers = None
+
+        producer = Producer(
+            {
+                "bootstrap.servers": output,
+                "compression.codec": kafka_settings["compression_type"],
+            }
+        )
+
+        item_exporter = KafkaItemExporter(
+            producer, kafka_settings["topic_map"], serializers
+        )
+
     elif item_exporter_type == ItemExporterType.CONSOLE:
         item_exporter = ConsoleItemExporter()
     else:
@@ -90,11 +160,12 @@ def determine_item_exporter_type(output):
     elif output is None or output == "console":
         return ItemExporterType.CONSOLE
     else:
-        return ItemExporterType.UNKNOWN
+        return ItemExporterType.KAFKA
 
 
 class ItemExporterType:
     PUBSUB = "pubsub"
     POSTGRES = "postgres"
     CONSOLE = "console"
+    KAFKA = "kafka"
     UNKNOWN = "unknown"
