cdd3cded1996a2cbab6232fa5a214f0f89ef285a
Richard Mah <dick.mah@gmail.com>
2021-04-23 16:05:20 -0400

    Bump version

--- iconetl/cli/__init__.py
+++ iconetl/cli/__init__.py
@@ -32,7 +32,7 @@ from iconetl.cli.stream import stream
 
 
 @click.group()
-@click.version_option(version="0.1.3")
+@click.version_option(version="0.1.4-alpha.2")
 @click.pass_context
 def cli(ctx):
     pass
