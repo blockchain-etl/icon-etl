d7ed09c4268076c6915275d0db16457f0a05d5be
Richard Mah <dick.mah@gmail.com>
2021-07-01 13:20:57 -0400

    Update copyright info (#17)

--- iconetl/jobs/exporters/postgres_item_exporter.py
+++ iconetl/jobs/exporters/postgres_item_exporter.py
@@ -1,6 +1,6 @@
 #  MIT License
 #
-#  Copyright (c) 2021 Richard Mah (richard@richardmah.com) & Insight Infrastructure
+#  Copyright (c) 2021 Richard Mah (richard@geometrylabs.io) & Geometry Labs (geometrylabs.io)
 #
 #  Permission is hereby granted, free of charge, to any person obtaining a copy of
 #  this software and associated documentation files (the "Software"), to deal in
